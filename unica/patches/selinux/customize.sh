1a0412843c69b12bfdc67d43b49a025a89aa7c37
nhglong <its.nhglong@gmail.com>
2026-04-23 23:36:50 +0700

    unica: patches: selinux: Remove unsupported entries for all .cils
    Match stock behaviour
    
    Change-Id: I2a4df554720d19269b02abcc33933fc49902fa62

--- unica/patches/selinux/customize.sh
+++ unica/patches/selinux/customize.sh
@@ -55,32 +55,39 @@ GET_SYSTEM_EXT()
     fi
 }
 
-CIL_NAME="$(head -n 1 "$WORK_DIR/vendor/etc/selinux/plat_sepolicy_vers.txt")"
-PATCHED=false
+CIL_LIST="$(find "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping" -type f -printf "%f\n" | \
+                    sed '/.compat./d' | sed 's/.cil//' | sort)"
 VENDOR_API_LIST="$(find "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping" -type f -printf "%f\n" | \
                     sed '/.compat./d' | sed 's/.cil//' | sed 's/\./_/' | sort)"
 # ]
 
+PATCHED=false
 for e in $ENTRIES; do
-    if grep -q -F "($e)" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil" || \
-         grep -q -F "${e}_${CIL_NAME//./_}" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"; then
-        # the problematic entry is currently present in system_ext, check if we need to remove it
-        if ! grep -q -F "(type $e)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
-            PATCHED=true
-            # the problematic entry is not supported by the target device
-            LOG "- \"$e\" SELinux entry not supported. Removing"
-            sed -i "/($e)/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
-            for a in $VENDOR_API_LIST; do
-                sed -i "/${e}_${a}/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
-            done
-            if grep -q "genfscon.*$e" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
-            fi
-            if grep -q "genfscon.*$e" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"; then
-                sed -i "/genfscon.*$e/d" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
+    SEEN=false
+    for CIL_NAME in $CIL_LIST; do
+        if grep -q -F "($e)" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil" || \
+            grep -q -F "${e}_${CIL_NAME//./_}" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"; then
+            # the problematic entry is currently present in system_ext, check if we need to remove it
+            if ! grep -q -F "(type $e)" "$WORK_DIR/vendor/etc/selinux/plat_pub_versioned.cil"; then
+                PATCHED=true
+                if ! $SEEN; then
+                    # the problematic entry is not supported by the target device
+                    LOG "- \"$e\" SELinux entry not supported. Removing"
+                    SEEN=true
+                fi
+                sed -i "/($e)/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+                for a in $VENDOR_API_LIST; do
+                    sed -i "/${e}_${a}/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/mapping/$CIL_NAME.cil"
+                done
+                if grep -q "genfscon.*$e" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"; then
+                    sed -i "/genfscon.*$e/d" "$WORK_DIR/$(GET_SYSTEM_EXT)/etc/selinux/system_ext_sepolicy.cil"
+                fi
+                if grep -q "genfscon.*$e" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"; then
+                    sed -i "/genfscon.*$e/d" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
+                fi
             fi
         fi
-    fi
+    done
 done
 
 for e in $DUPLICATES; do
@@ -99,5 +106,5 @@ if ! $PATCHED; then
     LOG "\033[0;33m! Nothing to do\033[0m"
 fi
 
-unset ENTRIES DUPLICATES CIL_NAME PATCHED VENDOR_API_LIST
+unset ENTRIES DUPLICATES CIL_LIST VENDOR_API_LIST PATCHED SEEN
 unset -f GET_SYSTEM_EXT
