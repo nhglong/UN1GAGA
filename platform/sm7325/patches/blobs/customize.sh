63e0212aa8f59e1afa8304f1a02a905bbb92addd
nhglong <its.nhglong@gmail.com>
2026-05-01 01:21:22 +0700

    platform: sm7325: Update gpu blobs
    Change-Id: Idbf3103be4b1413d55b1fe353ba4fc7c680a87e4

--- platform/sm7325/patches/blobs/customize.sh
+++ platform/sm7325/patches/blobs/customize.sh
@@ -114,3 +114,21 @@ if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "36" ]; then
     ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/saiv/image_understanding/db/hs_segmenter/hs_segmenter.tflite" 0 0 644 "u:object_r:vendor_configs_file:s0"
     ADD_TO_WORK_DIR "$SOURCE_FIRMWARE" "vendor" "etc/saiv/image_understanding/db/hs_segmenter/hs_segmenter.info" 0 0 644 "u:object_r:vendor_configs_file:s0"
 fi
+
+if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "36" ]; then
+    # Upgrade GPU blobs
+    for arch in lib lib64; do
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/eglSubDriverAndroid.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/libEGL_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/libGLESv1_CM_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/libGLESv2_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/libq3dtools_adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/egl/libq3dtools_esx.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/hw/vulkan.adreno.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/libadreno_utils.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/libCB.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/libgsl.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/libllvm-glnext.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    ADD_TO_WORK_DIR "a73xqxx" "vendor" "$arch/libllvm-qcom.so" 0 0 644 "u:object_r:same_process_hal_file:s0"
+    done
+fi
