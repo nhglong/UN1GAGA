ea79c421f700cf35fdddc0d7833d60490a7bd518
nhglong <its.nhglong@gmail.com>
2026-05-10 18:54:46 +0700

    unigaga: #110th Poseidon release
    Change-Id: I18ab13a8f6301169ee93a2c697cbb59f1a56e3bb

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -2,14 +2,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Only the below variable(s) need to be changed!
-VERSION_MAJOR=3
-VERSION_MINOR=2
+VERSION_MAJOR=1
+VERSION_MINOR=1
 VERSION_PATCH=0
 
 # The below variables will be generated automatically
 #
 # Version name
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+# Codename
+ROM_CODENAME="Poseidon"
 # Append "+" to version name if commits have been added since the last tag
 LATEST_TAG="$(git describe --tags --abbrev=0 2> /dev/null)"
 if [ "$LATEST_TAG" ]; then
