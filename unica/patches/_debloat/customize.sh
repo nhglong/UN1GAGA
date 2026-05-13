763eadb20cef8cc062f7feeeee68a380a67d6791
nhglong <its.nhglong@gmail.com>
2026-05-13 19:34:10 +0700

    unica: Support merging product partition to /system
    Change-Id: I4d4a6970182da1e320890c16084abadae3206609

--- unica/patches/_debloat/customize.sh
+++ unica/patches/_debloat/customize.sh
@@ -1,8 +1,6 @@
 # shellcheck disable=SC2016
 
 # Dexpreopt
-find "$WORK_DIR/product" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
-    bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "product" "${1//$WORK_DIR\/product\//}"' "bash" "{}"
 find "$WORK_DIR/system" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
     bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system" "${1//$WORK_DIR\/system\//}"' "bash" "{}"
 DELETE_FROM_WORK_DIR "system" "system/etc/boot-image.bprof"
@@ -11,6 +9,10 @@ DELETE_FROM_WORK_DIR "system" "system/framework/arm"
 DELETE_FROM_WORK_DIR "system" "system/framework/arm64"
 find "$WORK_DIR/system/system/framework" -type f -name "*.vdex" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
     bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system" "${1//$WORK_DIR\/system\//}"' "bash" "{}"
+if $TARGET_OS_BUILD_PRODUCT_PARTITION; then
+    find "$WORK_DIR/product" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
+        bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "product" "${1//$WORK_DIR\/product\//}"' "bash" "{}"
+fi
 if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
     find "$WORK_DIR/system_ext" -type d -name "oat" -print0 | xargs -0 -I "{}" -P "$(nproc)" \
         bash -c 'source "$SRC_DIR/scripts/utils/module_utils.sh"; DELETE_FROM_WORK_DIR "system_ext" "${1//$WORK_DIR\/system_ext\//}"' "bash" "{}"
